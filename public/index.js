b12d9b099ef44813f996810617a23e36dce8d9f0
Thomas Davidson <tomd1969@gmail.com>
2022-10-26 12:59:45 -0400

    Bonus completed.

--- public/index.js
+++ public/index.js
@@ -6,34 +6,34 @@ async function main() {
 
     // Save what we fetched to global variables;
     ingredients = fetchedIngredients
-    items = fetchedCartItems
+    cart.items = fetchedCartItems
 
     // Select the first sandwich on page load
-    selectSandwich(items[0])
-
+    //selectSandwich(items[0])
+    cart.selectSandwich(cart.items[0]);
     // Display the ingredients we fetched
-    renderIngredientList();
+    ingredientList.render();
 
     // Display the sandwiches we fetched
-    renderCart();
+    cart.renderCart();
 
     // Attach event listener to the 'Add Sandwich' button
     let addButton = document.querySelector('.add-button');
     addButton.addEventListener('click', () => {
-        addSandwich()
+        cart.addSandwich()
     })
 
     // Attach event listener to the 'Name' input
     let nameInput = document.querySelector('.name-input');
     nameInput.addEventListener('input', (e) => {
-        changeSelectedSandwichName(e.target.value)
+        cart.changeSelectedSandwichName(e.target.value)
     })
 
     // Attach event listeners to the radio inputs for selecting a type of bread
     let breadRadios = document.querySelectorAll('.bread-radio')
     breadRadios.forEach(radioInput => {
         radioInput.addEventListener('click', () => {
-            changeSelectedSandwichBread(radioInput.value)
+            cart.changeSelectedSandwichBread(radioInput.value)
         })
     })
 }
